6dce19361d51e669644d100a0ae45e4895b7535c
123 <user@pop-os.localdomain>
2023-10-03 16:02:14 +0100

    update: connection updated

---
+++ src/connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$host= 'db.khhmaxbegomuwdzkaxpj.supabase.co';
+$db = 'postgres';
+$user = 'postgres';
+$password = 'PHPSDKll@123'; 
+ 
--- src/index.php
+++ src/index.php
@@ -1,9 +1,21 @@
 <?php
-include 'config.php';
+require_once '../vendor/autoload.php';
+include 'connection.php';
 
-$conn = OpenCon();
-echo "Connected Succcessfully";
-CloseCon($conn);
+// user=postgres password=[YOUR-PASSWORD] host=db.khhmaxbegomuwdzkaxpj.supabase.co port=5432 dbname=postgres
 
-$sql = 'SELECT id, name,  FROM breads';
-?>
+try {
+	$dsn = "pgsql:host=$host;port=5432;dbname=$db;";
+	// make a database connection
+	$pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+	if ($pdo) {
+		echo "Connected to the $db database successfully!";
+	}
+} catch (PDOException $e) {
+	die($e->getMessage());
+} finally {
+	if ($pdo) {
+		$pdo = null;
+	}
+}
