c1c88df9dedf3f4508b3023cefe25590f63a8db5
yasermoamd <yasermoamd@hotmail.com>
2023-10-04 02:40:31 -0400

    updated

--- src/connection.php
+++ src/connection.php
@@ -1,7 +1,17 @@
-<?php
+<?php 
 
-$host= 'db.khhmaxbegomuwdzkaxpj.supabase.co';
-$db = 'postgres';
-$user = 'postgres';
-$password = 'PHPSDKll@123'; 
- 
+$host = getenv('DB_HOST'); 
+$dbname = getenv('DB_DATABASE_NAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$table_name = "breads";
+
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
+try {
+    $pdo = new PDO($dsn);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection Error: ' . $e->getMessage();
+    exit;
+}
+?>
--- src/index.php
+++ src/index.php
@@ -1,21 +1,59 @@
 <?php
-require_once '../vendor/autoload.php';
 include 'connection.php';
+require_once '../vendor/autoload.php';
 
-// user=postgres password=[YOUR-PASSWORD] host=db.khhmaxbegomuwdzkaxpj.supabase.co port=5432 dbname=postgres
+$sql = "SELECT * FROM \"$table_name\"";
+$result = $pdo->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-try {
-	$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$user;password=$password";
-	// make a database connection
-	$pdo = new PDO($dsn);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Display with Bootstrap</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
 
-	if ($pdo) {
-		echo "Connected to the $db database successfully!";
-	}
-} catch (PDOException $e) {
-	die($e->getMessage());
-} finally {
-	if ($pdo) {
-		$pdo = null;
-	}
-}
+<body>
+
+<div class="container mt-5">
+    <?php
+    if ($result) {
+        $data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!empty($data)) {
+            echo '<table class="table table-bordered table-hover">';
+            echo '<thead class="thead-dark">';
+            echo '<tr>';
+            foreach ($data[0] as $column => $value) {
+                echo '<th>' . $column . '</th>';
+            }
+            echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+
+            foreach ($data as $row) {
+                echo '<tr>';
+                foreach ($row as $value) {
+                    echo '<td>' . $value . '</td>';
+                }
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
+        } else {
+            echo '<div class="alert alert-warning">There are no data to display.</div>';
+        }
+    } else {
+        echo '<div class="alert alert-danger">Error exception.</div>';
+    }
+    ?>
+</div>
+
+<!-- Bootstrap JS and jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
